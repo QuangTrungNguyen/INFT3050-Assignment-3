c207e796d84b7249197013077e245475798a4754
Quang Nguyen <32567941+QuangTrungNguyen@users.noreply.github.com>
2018-10-25 15:08:57 +1100

    BusinessObjectLayer

---
+++ BusinessObjectLayer/BO_Titan.cs
@@ -0,0 +1,213 @@
+﻿namespace BusinessObjectLayer
+{
+    public class BO_Titan
+    {
+        private string username;
+        private string titanName;
+        private string element;
+        private int level;
+        private int step;
+        private int experiencePoints;
+        private int battles;
+        private int wins;
+        private int loses;
+        private string dateCreated;
+        private string hof;
+        private string pictureLocation;
+        private int selection;
+        private string screenName;
+        private string firstName;
+        private int userExercisePoints;
+        public string Username
+        {
+            get
+            {
+                return username;
+            }
+            set
+            {
+                username = value;
+            }
+        }
+
+        public string TitanName
+        {
+            get
+            {
+                return titanName;
+            }
+            set
+            {
+                titanName  = value;
+            }
+        }
+
+        public string Element
+        {
+            get
+            {
+                return element;
+            }
+            set
+            {
+                element = value;
+            }
+        }
+
+        public int Level
+        {
+            get
+            {
+                return level;
+            }
+            set
+            {
+                level = value;
+            }
+        }
+
+        public int ExperiencePoints
+        {
+            get
+            {
+                return experiencePoints;
+            }
+            set
+            {
+                experiencePoints = value;
+            }
+        }
+
+        public int Step
+        {
+            get
+            {
+                return step;
+            }
+            set
+            {
+                step = value;
+            }
+        }
+
+        public int Battles
+        {
+            get
+            {
+                return battles;
+            }
+            set
+            {
+                battles = value;
+            }
+        }
+
+        public int Wins
+        {
+            get
+            {
+                return wins;
+            }
+            set
+            {
+                wins = value;
+            }
+        }
+
+        public int Loses
+        {
+            get
+            {
+                return loses;
+            }
+            set
+            {
+                loses = value;
+            }
+        }
+
+        public string DateCreated
+        {
+            get
+            {
+                return dateCreated;
+            }
+            set
+            {
+                dateCreated = value;
+            }
+        }
+
+        public string HOF
+        {
+            get
+            {
+                return hof;
+            }
+            set
+            {
+                hof = value;
+            }
+        }
+
+        public string PictureLocation
+        {
+            get
+            {
+                return pictureLocation;
+            }
+            set
+            {
+                pictureLocation = value;
+            }
+        }
+
+        public int Selection
+        {
+            get
+            {
+                return selection;
+            }
+            set
+            {
+                selection = value;
+            }
+        }
+
+        public string ScreenName
+        {
+            get
+            {
+                return screenName;
+            }
+            set
+            {
+                screenName = value;
+            }
+        }
+
+        public string FirstName
+        {
+            get
+            {
+                return firstName;
+            }
+            set
+            {
+               firstName = value;
+            }
+        }
+
+        public int UserExercisePoints
+        {
+            get
+            {
+                return userExercisePoints;
+            }
+            set
+            {
+                userExercisePoints = value;
+            }
+        }
+    }
+}
---
+++ BusinessObjectLayer/BO_User.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessObjectLayer
+{
+    public class BO_User
+    {
+        // Claim variables
+        private string username;
+        private string password;
+        private string parentsEmail;
+        private int exercisePoints;
+        private int tempExercisePoints;
+        private string activateStatus;
+        private int activateCode;
+        private string profilePictureLocation;
+        private string screenName;
+        private string anonymous;
+        private string firstName;
+        private int oneDayExercisePoints;
+        private string oneDayStartTime;
+        private int uid;
+        // Get and set variables
+        public string Username
+        {
+            get
+            {
+                return username;
+            }
+            set
+            {
+                username = value;
+            }
+        }
+
+        public string Password
+        {
+            get
+            {
+                return password;
+            }
+            set
+            {
+                password = value;
+            }
+        }
+
+        public string ParentsEmail
+        {
+            get
+            {
+                return parentsEmail;
+            }
+            set
+            {
+                parentsEmail = value;
+            }
+        }
+        public int ExercisePoints
+        {
+            get
+            {
+                return exercisePoints;
+            }
+            set
+            {
+                exercisePoints = value;
+            }
+        }
+
+        public int TempExercisePoints
+        {
+            get
+            {
+                return tempExercisePoints;
+            }
+            set
+            {
+                tempExercisePoints = value;
+            }
+        }
+
+        public String ActivateStatus
+        {
+            get
+            {
+                return activateStatus;
+            }
+            set
+            {
+                activateStatus = value;
+            }
+        }
+
+        public int ActivateCode
+        {
+            get
+            {
+                return activateCode;
+            }
+            set
+            {
+                activateCode = value;
+            }
+        }
+
+        public string ProfilePictureLocation
+        {
+            get
+            {
+                return profilePictureLocation;
+            }
+            set
+            {
+                profilePictureLocation = value;
+            }
+        }
+
+        public string ScreenName
+        {
+            get
+            {
+                return screenName;
+            }
+            set
+            {
+                screenName = value;
+            }
+        }
+
+        public string Anonymous
+        {
+            get
+            {
+                return anonymous;
+            }
+            set
+            {
+                anonymous = value;
+            }
+        }
+
+        public string FirstName
+        {
+            get
+            {
+                return firstName;
+            }
+            set
+            {
+                firstName = value;
+            }
+        }
+
+        public int OneDayExercisePoints
+        {
+            get
+            {
+                return oneDayExercisePoints;
+            }
+            set
+            {
+                oneDayExercisePoints = value;
+            }
+        }
+
+        public string OneDayStartTime
+        {
+            get
+            {
+                return oneDayStartTime;
+            }
+            set
+            {
+                oneDayStartTime = value;
+            }
+        }
+
+        public int Uid
+        {
+            get
+            {
+                return uid;
+            }
+            set
+            {
+               uid = value;
+            }
+        }
+    }
+}
